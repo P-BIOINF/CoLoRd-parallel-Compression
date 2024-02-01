71d324a56ccd04a4fff37e2287cde5d445e9aa27
xyz <xyz@DESKTOP-IGLT8MF>
2024-02-01 14:32:17 +0100

    Dodane streamy

--- Streams.h
+++ Streams.h
@@ -6,59 +6,59 @@
 class Streams
 {
 private:
-	std::ifstream m_input{};
-	std::ofstream m_output{};
-	std::ofstream m_logs{};
+    std::ifstream m_input{};
+    std::ofstream m_output{};
+    std::ofstream m_logs{};
 
 public:
-	Streams() = default;
+    Streams() = default;
 
-	Streams(const std::string& input, const std::string& logs)
-		: m_input{ input }, m_logs{ logs } {}
+    Streams(const std::string& input, const std::string& logs)
+        : m_input{ input }, m_logs{ logs } {}
 
-	bool setInput(const std::string& input)
-	{
-		if (m_input.is_open())
-			m_input.close();
-		m_input.open(input);
-		return m_input.good();
-	}
+    bool setInput(const std::string& input)
+    {
+        if (m_input.is_open())
+            m_input.close();
+        m_input.open(input);
+        return m_input.good();
+    }
 
-	bool setLogs(const std::string& logs)
-	{
-		if (m_logs.is_open())
-			m_logs.close();
-		m_logs.open(logs);
-		return m_logs.good();
-	}
+    bool setLogs(const std::string& logs)
+    {
+        if (m_logs.is_open())
+            m_logs.close();
+        m_logs.open(logs);
+        return m_logs.good();
+    }
 
-	bool setOutput(const std::string& output)
-	{
-		if (m_output.is_open())
-			m_output.close();
-		m_out.open(output);
-		return m_output.good();
-	}
+    bool setOutput(const std::string& output)
+    {
+        if (m_output.is_open())
+            m_output.close();
+        m_out.open(output);
+        return m_output.good();
+    }
 
-	std::ifstream& getInputStream()
-	{
-		return m_input;
-	}
+    std::ifstream& getInputStream()
+    {
+        return m_input;
+    }
 
-	std::ofstream& getOutputStream()
-	{
-		return m_output;
-	}
+    std::ofstream& getOutputStream()
+    {
+        return m_output;
+    }
 
-	std::ofstream& getLogsStream()
-	{
-		return m_logs;
-	}
+    std::ofstream& getLogsStream()
+    {
+        return m_logs;
+    }
 
-	[[nodiscard]] bool good() const
-	{
-		return m_input.good() && m_output.good() && m_logs.good();
-	}
+    [[nodiscard]] bool good() const
+    {
+        return m_input.good() && m_output.good() && m_logs.good();
+    }
 
 };
 
