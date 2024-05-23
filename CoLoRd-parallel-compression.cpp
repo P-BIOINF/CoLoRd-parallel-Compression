9e5ba297df74a8a753841a61c055310387441ab9
Casper9474 <146725742+Casper9474@users.noreply.github.com>
2024-05-23 22:08:12 +0200

    Deleted not used features

--- CoLoRd-parallel-compression.cpp
+++ CoLoRd-parallel-compression.cpp
@@ -4,27 +4,33 @@
 
 int main(const int argc, char** argv)
 {
+	std::ios_base::sync_with_stdio(false);
+
 	const std::string announcement{ "Something went wrong! Please make sure that you have included:\n"
 		"--output <output directory> --input <input directory> --colord <colord directory> -m <colord mode> --lpthread <maximum number of threads> --maxFiles <max number of files to output> --count <sequences how often to change the file>\n" };
-	std::ios_base::sync_with_stdio(false);
+
 	Parallel parallel{};
+
 	if (parallel.parseArguments(argc, argv) != Status::ready)
 	{
 		std::cerr << "Status code: " << static_cast<int>(parallel.getStatus()) << '\n';
 		std::cerr << announcement;
 		return -1;
 	}
+
 	if (!parallel.getInputStream().good())
 	{
 		std::cerr << announcement;
 		return -2;
 	}
-	parallel.calculateCount();
+
 	if (!parallel.createFiles())
 	{
 		std::cerr << announcement;
 		return -3;
 	}
+
 	parallel.compress();
+
 	return 0;
 }
